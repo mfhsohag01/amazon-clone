e349fc181aee43221e546dc6dc52331c344d1a3e
mfhsohag01 <md.sohag2001bd@gmail.com>
2025-01-03 12:05:21 +0600

    morning mini change

---
+++ script.js
@@ -0,0 +1,12 @@
+const imgs = document.querySelectorAll(".header-slider ul img");
+const prev_btn = document.querySelectorAll(".control_prev");
+const next_btn = document.querySelectorAll(".control_next");
+
+let n = 0;
+
+function changeSlide() {
+  for (let i = 0; i < imgs.length; i++) {
+    imgs[i].style.display = "none";
+  }
+}
+changeSlide();
