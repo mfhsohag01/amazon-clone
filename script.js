ae4273693424c5614b97c91324939f3d05221183
mfhsohag01 <md.sohag2001bd@gmail.com>
2025-01-03 19:46:17 +0600

    simle error distroyed my 1 hour

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const imgs = document.querySelectorAll(".header-slider ul img");
-const prev_btn = document.querySelectorAll(".control_prev");
-const next_btn = document.querySelectorAll(".control_next");
+const prev_btn = document.querySelector(".control-prev");
+const next_btn = document.querySelector(".control-next");
 
 let n = 0;
 
@@ -8,5 +8,25 @@ function changeSlide() {
   for (let i = 0; i < imgs.length; i++) {
     imgs[i].style.display = "none";
   }
+  imgs[n].style.display = "block";
 }
+
 changeSlide();
+
+prev_btn.addEventListener("click", (e) => {
+  if (n > 0) {
+    n--;
+  } else {
+    n = imgs.length - 1;
+  }
+  changeSlide();
+});
+
+next_btn.addEventListener("click", (e) => {
+  if (n < imgs.length - 1) {
+    n++;
+  } else {
+    n = 0;
+  }
+  changeSlide();
+});
