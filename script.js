a740286102ed05eb74d2f29fb73ec0ca4d1dd9ed
mfhsohag01 <md.sohag2001bd@gmail.com>
2025-01-04 13:00:59 +0600

    amazon homepage full design

--- script.js
+++ script.js
@@ -30,3 +30,11 @@ next_btn.addEventListener("click", (e) => {
   }
   changeSlide();
 });
+
+const scrollContainer = document.querySelectorAll(".products");
+for (const item of scrollContainer) {
+  item.addEventListener("wheel", (event) => {
+    event.preventDefault();
+    item.scrollLeft += event.deltaY;
+  });
+}
